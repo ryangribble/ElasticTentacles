ea0a2d69d0f4dc8040079825d7e0ddf46a7ebdce
Ryan Gribble <ryan.gribble@gmail.com>
2016-05-15 20:31:19 +1000

    add scripts

---
+++ scripts/CreateTentacle.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = "Stop" 
+
+$octopusUrl = "http://localhost"
+$apiKey = "API-YDZ26JLZD2QRUK3YP9VFUTRYR0"
+$role = "api"
+
+#$environment = $OctopusParameters['Octopus.Environment.Name']
+$environment = "Dev"
+
+#$numTentacles = $OctopusParameters['NumberOfTentacles']
+$numTentacles = 1
+
+$baseName = "Tentacle"
+$basePort = 10980
+
+$baseName = $baseName + "_" + $(Get-Date).ToString("yyyyMMdd_HHmm")
+
+
+for ($tentacleNo = 1; $tentacleNo -le $numTentacles; $tentacleNo++)
+{
+    $name = $baseName + "_" + $tentacleNo
+    $port = $basePort + $tentacleNo - 1
+
+    #Write-Output "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" register-with --instance "$name" --name "$name" --publicHostName "https://cd:$port" --server "$octopusUrl" --apiKey="$apiKey" --role "$role" --environment "$environment" --comms-style TentaclePassive --console  --policy="The Cattle Yard"
+    #return
+    
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" create-instance --instance "$name" --config "C:\Octopus\$name\Tentacle-$name.config" --console
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" new-certificate --instance "$name" --if-blank --console
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "$name" --reset-trust --console
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "$name" --home "C:\Octopus\$name" --app "C:\Octopus\Applications\$name" --port "$port" --noListen "False"  --console
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" configure --instance "$name" --trust "E59BD2E42B886007DD0CBA48C633F18CD48FD75F"  --console
+    & "netsh" advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=$port
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" register-with --instance "$name" --name "$name" --publicHostName "cd" --server "$octopusUrl" --apiKey="$apiKey" --role "$role" --environment "$environment" --comms-style TentaclePassive --console  --policy="The Cattle Yard"
+    & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance "$name" --install --start  --console
+}
---
+++ scripts/DeleteTentacle.ps1
@@ -0,0 +1,13 @@
+﻿function Delete-Tentacles([string]$baseName, [int]$numTentacles)
+{
+
+    for ($tentacleNo = 1; $tentacleNo -le $numTentacles; $tentacleNo++)
+    {
+        $name = $baseName + $tentacleNo
+        & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" service --instance "$name" --stop --uninstall
+        & "C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe" delete-instance --instance "$name"
+    }
+}
+
+Delete-Tentacles "Tentacle_20160514_2208_" 2
+#Delete-Tentacles "Tentacle_20160514_2319_" 8
